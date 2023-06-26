684770c7fe09b4eba2bb7de5f586be71326fe766
Jeff Hollan <jeff.hollan@snowflake.com>
2023-06-26 14:07:02 -0700

    add a filter

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ from snowflake.snowpark.session import Session
 from snowflake.snowpark import DataFrame
 from snowflake.snowpark.functions import col
 
+
 def hello(session: Session) -> DataFrame:
     df = session.table("products")
-    return df
+    return df.filter(col("ID") == 1)
+
 
 # For local debugging
 if __name__ == "__main__":
     from snowflake.snowpark.mock.mock_connection import MockServerConnection
     import init_local
 
-    session = Session(MockServerConnection()) # type: ignore
+    session = Session(MockServerConnection())  # type: ignore
     session = init_local.init(session)
     print(hello(session).show())
